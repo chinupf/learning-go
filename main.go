193960472b8acc0b2acb232fb8189fd5a14f418e
Johannes <johannes.scharfenberg@gmail.com>
2024-08-26 09:49:17 +0200

    yo

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ import (
 	"time"
 )
 
+func printer() {
+	if r%2 == 0 {
+		fmt.Println(stars)
+	} else {
+		fmt.Println(plus)
+	}
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(5) + 1
 	stars := strings.Repeat("*", r)
-	fmt.Println(stars)
+	plus := strings.Repeat("+", r)
+	printer
 }
