dfc5b28541da14bde2bed5cbf0e4b69860b1ed73
Johannes <johannes.scharfenberg@gmail.com>
2024-09-02 13:06:41 +0200

    start

---
+++ e104/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	Debug       bool
+	LogLevel    = "info"
+	startUpTime = time.Now()
+)
+
+func main() {
+
+	fmt.Println(Debug, LogLevel, startUpTime)
+}
+
