1b4ee2af991e0cd7551d5bcd715fb46afdddc6d2
Johannes <johannes.scharfenberg@gmail.com>
2024-09-26 10:27:48 +0200

    yo

--- e105/main.go
+++ e105/main.go
@@ -6,9 +6,6 @@ import (
 )
 
 func main() {
-	Debug := false
-	LogLevel := info
-	startUpTime := time.Now()
-
+	Debug, LogLevel, startUpTime := false, "info", time.Now()
 	fmt.Println(Debug, LogLevel, startUpTime)
 }
